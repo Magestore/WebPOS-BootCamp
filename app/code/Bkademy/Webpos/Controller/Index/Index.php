5617e56390853406e799eb3455cc533d02c7b27b
tuann <ngoctuan.nguyen@trueplus.vn>
2016-12-12 21:12:58 +0700

    Hello World

---
+++ app/code/Bkademy/Webpos/Controller/Index/Index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Copyright © 2016 Bkademy. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Bkademy\Webpos\Controller\Index;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    public function execute()
+    {
+        echo 'Hello World!';
+    }
+}
