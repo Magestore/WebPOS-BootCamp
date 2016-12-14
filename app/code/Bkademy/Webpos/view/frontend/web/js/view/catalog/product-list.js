9fb0d09d2ae2f076acf670155a084d5882b3b9c5
Eden Duong <quocviet.duong@trueplus.vn>
2016-12-14 21:31:57 +0700

    product

--- app/code/Bkademy/Webpos/view/frontend/web/js/view/catalog/product-list.js
+++ app/code/Bkademy/Webpos/view/frontend/web/js/view/catalog/product-list.js
@@ -3,13 +3,37 @@
  * See COPYING.txt for license details.
  */
 define([
-    'uiComponent'
-], function (Component) {
+    'uiComponent',
+    'mage/storage',
+    'Bkademy_Webpos/js/model/url-builder'
+], function (Component, storage, urlBuilder) {
     'use strict';
 
     return Component.extend({
+
+
         defaults: {
             template: 'Bkademy_Webpos/catalog/product-list'
+        },
+
+        initialize: function () {
+            var self = this;
+            this._super();
+            self.showProduct(1);
+        },
+
+        showProduct: function (pageNumber) {
+            var self = this;
+            var params = {};
+            var serviceUrl = urlBuilder.createUrl('/products?searchCriteria[pageSize]=20&searchCriteria[currentPage]='+pageNumber, params);
+            var payload = {};
+            storage.get(
+                serviceUrl, JSON.stringify(payload)
+            ).done(function (response) {
+
+            }).fail(function (response) {
+
+            });
         }
     });
 });
