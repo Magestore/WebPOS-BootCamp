28fb91f1a34fcc71d8210d7c4212209b70cd5f18
Lee <duongnguyen.it92@gmail.com>
2016-12-14 08:26:19 +0700

    Update Index

--- app/code/Bkademy/Webpos/Controller/Index/Index.php
+++ app/code/Bkademy/Webpos/Controller/Index/Index.php
@@ -9,34 +9,6 @@ class Index extends \Magento\Framework\App\Action\Action
 {
     public function execute()
     {
-        /**
-         * @var \Magento\Sales\Api\OrderRepositoryInterface $orderRepository
-         */
-//        $orderRepository = $this->_objectManager->get('Magento\Sales\Api\OrderRepositoryInterface');
-//
-//        /**
-//         * @var \Magento\Framework\Api\SearchCriteriaBuilder $searchCriteriaBuilder
-//         */
-//        $searchCriteriaBuilder = $this->_objectManager->get('Magento\Framework\Api\SearchCriteriaBuilder');
-//        $searchCriteria = $searchCriteriaBuilder->create();
-//        $searchCriteria->setPageSize(10);
-//        $searchCriteria->setCurrentPage(1);
-//        $orders = $orderRepository->getList($searchCriteria);
-
-        $productRepository = $this->_objectManager->get('Bkademy\Webpos\Api\Catalog\ProductRepositoryInterface');
-
-        /**
-         * @var \Magento\Framework\Api\SearchCriteriaBuilder $searchCriteriaBuilder
-         */
-        $searchCriteriaBuilder = $this->_objectManager->get('Magento\Framework\Api\SearchCriteriaBuilder');
-        $searchCriteria = $searchCriteriaBuilder->create();
-        $searchCriteria->setPageSize(20);
-        $searchCriteria->setCurrentPage(1);
-        $product = $productRepository->getList($searchCriteria);
-
-
-        $json = \Zend_Json::encode($product);
-
-        echo $json;
+        echo 'Hello World!';
     }
 }
