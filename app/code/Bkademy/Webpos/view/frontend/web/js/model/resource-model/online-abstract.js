40985bffe012c42f634ac21f591f3b821f165942
Lee <duongnguyen.it92@gmail.com>
2016-12-14 22:19:32 +0700

    Order

---
+++ app/code/Bkademy/Webpos/view/frontend/web/js/model/resource-model/online-abstract.js
@@ -0,0 +1,20 @@
+/*
+ *  Copyright © 2016 Bkademy. All rights reserved.
+ *  See COPYING.txt for license details.
+ *
+ */
+
+define(
+    [
+        'jquery',
+        'Bkademy_Webpos/js/model/resource-model/abstract'
+    ],
+    function ($, resourceAbstract) {
+        "use strict";
+
+        return resourceAbstract.extend({
+
+
+        });
+    }
+);
