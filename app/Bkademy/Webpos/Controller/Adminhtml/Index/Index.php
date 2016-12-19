61b2cfa47e6da0374f6ac1546ced2f0464624d17
htmyheart <tdng91@gmail.com>
2016-12-19 22:49:55 +0700

    new code

---
+++ app/Bkademy/Webpos/Controller/Adminhtml/Index/Index.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Bkademy\Webpos\Controller\Adminhtml\Index;
+
+class Index extends \Magento\Backend\App\Action
+{
+    public function execute()
+    {
+        echo "Hello world, this is backend";
+    }
+}
